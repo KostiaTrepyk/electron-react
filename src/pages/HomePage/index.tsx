91d469e76871d8c30b81514baffc098569309c1f
KostiaTrepyk <137499840+KostiaTrepyk@users.noreply.github.com>
2024-01-19 21:13:49 +0100

    Added password hiding and improved password generation.

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,16 +1,30 @@
 import React, { useState } from "react";
-import { Box, Button, IconButton, TextField, Typography } from "@mui/material";
-import { ContentCopy as ContentCopyICon } from "@mui/icons-material";
+import { Box, Button, IconButton, Tooltip, Typography } from "@mui/material";
+import {
+  ContentCopy as ContentCopyICon,
+  Settings as SettingsIcon,
+} from "@mui/icons-material";
 
 import Navbar from "../../components/Navbar";
+import SettingsMenu from "../../components/SettingsMenu";
 
 const HomePage = () => {
-  const [passwordLength, setPasswordLength] = useState<number>(5);
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [passwordLength, setPasswordLength] = useState<number>(8);
   const [generatedPassword, setGeneratedPassword] = useState<string>("");
+  const [isSettingOpened, setIsSettingOpened] = useState<boolean>(false);
+  const [charactersType, setCharactersType] = useState<
+    "alphabetic" | "alphanumeric" | "numeric"
+  >("alphanumeric");
 
   function generatePassword() {
-    let charset =
-      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" + "0123456789";
+    const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const nums = "0123456789";
+
+    let charset = "";
+    if (charactersType === "alphabetic") charset = letters;
+    else if (charactersType === "numeric") charset = nums;
+    else charset = letters + nums;
 
     let result = "";
 
@@ -32,9 +46,11 @@ const HomePage = () => {
         flexDirection: "column",
         height: "100dvh",
         width: "100dvw",
+        position: "relative",
       }}
     >
       <Navbar />
+
       <Box
         style={{
           flexGrow: 1,
@@ -54,24 +70,33 @@ const HomePage = () => {
         >
           {Boolean(generatedPassword) ? (
             <Box sx={{ display: "flex", gap: 1, alignItems: "center" }}>
-              <Typography
-                sx={{
-                  maxWidth: "50dvw",
-                  whiteSpace: "nowrap",
-                  textOverflow: "ellipsis",
-                  overflow: "hidden",
-                }}
-                variant="h5"
-                color="white"
-              >
-                {generatedPassword}
-              </Typography>
-              <IconButton
-                color="success"
-                onClick={() => CopyToClipboard(generatedPassword)}
-              >
-                <ContentCopyICon />
-              </IconButton>
+              <Tooltip title={showPassword ? "Hide" : "Reveal"}>
+                <Typography
+                  sx={{
+                    maxWidth: "50dvw",
+                    whiteSpace: "nowrap",
+                    textOverflow: "ellipsis",
+                    overflow: "hidden",
+                    cursor: "pointer",
+                  }}
+                  variant="h5"
+                  color="white"
+                  onClick={() => setShowPassword((prev) => !prev)}
+                >
+                  {showPassword
+                    ? generatedPassword
+                    : new Array(generatedPassword.length).fill("∗")}
+                </Typography>
+              </Tooltip>
+
+              <Tooltip title="Copy">
+                <IconButton
+                  color="success"
+                  onClick={() => CopyToClipboard(generatedPassword)}
+                >
+                  <ContentCopyICon />
+                </IconButton>
+              </Tooltip>
             </Box>
           ) : (
             <Typography variant="h5" color={"white"}>
@@ -83,26 +108,37 @@ const HomePage = () => {
         <Box
           sx={{
             display: "flex",
-            justifyContent: "center",
             gap: 2,
+            width: "fit-content",
+            marginInline: "auto",
           }}
         >
-          <TextField
-            variant="standard"
-            label="Length"
-            type="number"
-            value={passwordLength}
-            onChange={(e) => setPasswordLength(Number(e.currentTarget.value))}
-          />
-
           <Button
+            sx={{ width: "fit-content" }}
             variant="contained"
             color="primary"
             onClick={generatePassword}
           >
-            Create
+            Generate
           </Button>
+
+          <IconButton
+            sx={{ marginInline: "auto" }}
+            size="large"
+            onClick={() => setIsSettingOpened((prev) => !prev)}
+          >
+            <SettingsIcon />
+          </IconButton>
         </Box>
+
+        <SettingsMenu
+          open={isSettingOpened}
+          onClose={() => setIsSettingOpened(false)}
+          passwordLength={passwordLength}
+          onChangePasswordLength={(newLength) => setPasswordLength(newLength)}
+          charactersType={charactersType}
+          onChangeCharactersType={(newType) => setCharactersType(newType)}
+        />
       </Box>
     </Box>
   );
