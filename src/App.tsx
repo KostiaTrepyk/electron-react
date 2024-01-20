44cfb3b25850fe2e544724a7e85b14a7ec4faa3a
KostiaTrepyk <137499840+KostiaTrepyk@users.noreply.github.com>
2024-01-20 19:57:18 +0100

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "@emotion/react";
-import HomePage from "./pages/HomePage";
 import { theme } from "./core/theme";
+import Router from "./core/router/components/Router";
+import HomePage from "./pages/HomePage";
+import HelpPage from "./pages/HelpPage";
 
 import "./styles/index.css";
+import PasswordContextProvider from "./contexts/PasswordContext/Provider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <HomePage />
+      <PasswordContextProvider>
+        <Router
+          routes={[
+            { path: "/", element: <HomePage /> },
+            { path: "/help", element: <HelpPage /> },
+          ]}
+          defaultElement={<HomePage />}
+        />
+      </PasswordContextProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
