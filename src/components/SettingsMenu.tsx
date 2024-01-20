997da3089f44f602d4c68ae5067ffa5c9864dd6a
KostiaTrepyk <137499840+KostiaTrepyk@users.noreply.github.com>
2024-01-20 19:54:21 +0100

    SettingsMenu improvement

--- src/components/SettingsMenu.tsx
+++ src/components/SettingsMenu.tsx
@@ -1,72 +1,108 @@
 import React from "react";
 import {
   Box,
+  Button,
   Drawer,
+  FormControlLabel,
   IconButton,
   MenuItem,
   Select,
+  Switch,
   TextField,
+  Tooltip,
 } from "@mui/material";
-import { ArrowBackIosNew as ArrowBackIcon } from "@mui/icons-material";
+import {
+  ArrowBackIosNew as ArrowBackIcon,
+  QuestionMark as QuestionMarkIcon,
+  SafetyCheck as SafetyCheckIcon,
+} from "@mui/icons-material";
+import { useRouterContext } from "../core/router/hooks/useRouterContext";
+import { usePasswordContext } from "../contexts/PasswordContext/usePasswordContext";
 
 interface SettingsMenuProps {
   open: boolean;
   onClose: () => void;
-  passwordLength: number;
-  onChangePasswordLength: (newLength: number) => void;
-  charactersType: "alphabetic" | "alphanumeric" | "numeric";
-  onChangeCharactersType: (
-    newType: "alphabetic" | "alphanumeric" | "numeric"
-  ) => void;
 }
 
-const SettingsMenu: React.FC<SettingsMenuProps> = ({
-  open,
-  onClose,
-  passwordLength,
-  onChangePasswordLength,
-  charactersType,
-  onChangeCharactersType,
-}) => {
+const SettingsMenu: React.FC<SettingsMenuProps> = ({ open, onClose }) => {
+  const { navigate } = useRouterContext();
+  const {
+    passwordLength,
+    charsType,
+    isSpecialChars,
+
+    changePasswordLength,
+    changeCharsType,
+    changeIsSpecialChars,
+    generateNewPassword,
+  } = usePasswordContext();
+
   return (
     <Drawer anchor="bottom" open={open} onClose={onClose} variant="persistent">
-      <IconButton
-        sx={{
-          mt: 1,
-          width: "fit-content",
-          marginInline: "auto",
-        }}
-        onClick={onClose}
-      >
+      <Button size="small" onClick={onClose} color="inherit" fullWidth>
         <ArrowBackIcon sx={{ rotate: "-90deg" }} />
-      </IconButton>
+      </Button>
 
       <Box
         sx={{
           display: "flex",
           justifyContent: "center",
           gap: 2,
-          padding: "5% 0",
+          padding: "7dvh 0",
+          flexWrap: "wrap",
+          width: "100%",
         }}
       >
+        <Tooltip title="Generate password">
+          <IconButton
+            sx={{ height: 50, width: 50 }}
+            onClick={generateNewPassword}
+          >
+            <SafetyCheckIcon />
+          </IconButton>
+        </Tooltip>
+
         <TextField
           variant="standard"
           label="Length"
           type="number"
           value={passwordLength}
-          onChange={(e) =>
-            onChangePasswordLength(Number(e.currentTarget.value))
-          }
+          onChange={(e) => {
+            const newValue = Number(e.currentTarget.value);
+
+            if (newValue < 1 || newValue > 100) return;
+            changePasswordLength(Number(e.currentTarget.value));
+          }}
         />
 
         <Select
-          value={charactersType}
-          onChange={(e) => onChangeCharactersType(e.target.value as any)}
+          variant="outlined"
+          value={charsType}
+          onChange={(e) => changeCharsType(e.target.value as any)}
         >
           <MenuItem value={"alphabetic"}>Alphabetic</MenuItem>
           <MenuItem value={"alphanumeric"}>Alphanumeric</MenuItem>
           <MenuItem value={"numeric"}>Numeric</MenuItem>
         </Select>
+
+        <FormControlLabel
+          control={
+            <Switch
+              checked={isSpecialChars}
+              onChange={(_, checked) => changeIsSpecialChars(checked)}
+            />
+          }
+          label="Special chars"
+        />
+
+        <Tooltip title="Help">
+          <IconButton
+            sx={{ height: 50, width: 50 }}
+            onClick={() => navigate("/help")}
+          >
+            <QuestionMarkIcon />
+          </IconButton>
+        </Tooltip>
       </Box>
     </Drawer>
   );
