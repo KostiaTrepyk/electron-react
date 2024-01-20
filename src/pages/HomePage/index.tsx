47d8589fd2328b51f40395e80e1efd7e959ee0b4
KostiaTrepyk <137499840+KostiaTrepyk@users.noreply.github.com>
2024-01-20 19:57:44 +0100

    HomePage improvements

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,146 +1,112 @@
-import React, { useState } from "react";
+import React from "react";
 import { Box, Button, IconButton, Tooltip, Typography } from "@mui/material";
 import {
   ContentCopy as ContentCopyICon,
   Settings as SettingsIcon,
 } from "@mui/icons-material";
 
-import Navbar from "../../components/Navbar";
+import { usePasswordContext } from "../../contexts/PasswordContext/usePasswordContext";
+import Layout from "../../components/Layout";
 import SettingsMenu from "../../components/SettingsMenu";
 
 const HomePage = () => {
-  const [showPassword, setShowPassword] = useState<boolean>(false);
-  const [passwordLength, setPasswordLength] = useState<number>(8);
-  const [generatedPassword, setGeneratedPassword] = useState<string>("");
-  const [isSettingOpened, setIsSettingOpened] = useState<boolean>(false);
-  const [charactersType, setCharactersType] = useState<
-    "alphabetic" | "alphanumeric" | "numeric"
-  >("alphanumeric");
+  const [isSettingOpened, setIsSettingOpened] = React.useState<boolean>(false);
+  const {
+    showPassword,
+    passwordLength,
+    generatedPassword,
+    charsType,
+    isSpecialChars,
 
-  function generatePassword() {
-    const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const nums = "0123456789";
-
-    let charset = "";
-    if (charactersType === "alphabetic") charset = letters;
-    else if (charactersType === "numeric") charset = nums;
-    else charset = letters + nums;
-
-    let result = "";
-
-    for (var i = 0, n = charset.length; i < passwordLength; ++i) {
-      result += charset.charAt(Math.floor(Math.random() * n));
-    }
-
-    setGeneratedPassword(result);
-  }
+    changeShowPassword,
+    changePasswordLength,
+    changeCharsType,
+    changeIsSpecialChars,
+    generateNewPassword,
+  } = usePasswordContext();
 
   function CopyToClipboard(str: string) {
     navigator.clipboard.writeText(str);
   }
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        height: "100dvh",
-        width: "100dvw",
-        position: "relative",
-      }}
-    >
-      <Navbar />
-
+    <Layout>
       <Box
-        style={{
+        sx={{
           flexGrow: 1,
-          overflowY: "auto",
           display: "flex",
-          flexDirection: "column",
-          padding: "5%",
+          justifyContent: "center",
+          alignItems: "center",
         }}
       >
-        <Box
-          sx={{
-            flexGrow: 1,
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
+        {Boolean(generatedPassword) ? (
+          <Box sx={{ display: "flex", gap: 1, alignItems: "center" }}>
+            <Tooltip title={showPassword ? "Hide" : "Reveal"}>
+              <Typography
+                sx={{
+                  maxWidth: "50dvw",
+                  whiteSpace: "nowrap",
+                  textOverflow: "ellipsis",
+                  overflow: "hidden",
+                  cursor: "pointer",
+                }}
+                variant="h5"
+                onClick={() => changeShowPassword(!showPassword)}
+              >
+                {showPassword
+                  ? generatedPassword
+                  : new Array(generatedPassword.length).fill("∗")}
+              </Typography>
+            </Tooltip>
+
+            <Tooltip title="Copy">
+              <IconButton
+                color="success"
+                onClick={() => CopyToClipboard(generatedPassword)}
+              >
+                <ContentCopyICon />
+              </IconButton>
+            </Tooltip>
+          </Box>
+        ) : (
+          <Typography variant="h5">Password generator</Typography>
+        )}
+      </Box>
+
+      <Box
+        sx={{
+          display: "flex",
+          gap: 2,
+          paddingBottom: "7dvh",
+          width: "fit-content",
+          marginInline: "auto",
+        }}
+      >
+        <Button
+          sx={{ width: "fit-content" }}
+          variant="contained"
+          color="primary"
+          onClick={generateNewPassword}
         >
-          {Boolean(generatedPassword) ? (
-            <Box sx={{ display: "flex", gap: 1, alignItems: "center" }}>
-              <Tooltip title={showPassword ? "Hide" : "Reveal"}>
-                <Typography
-                  sx={{
-                    maxWidth: "50dvw",
-                    whiteSpace: "nowrap",
-                    textOverflow: "ellipsis",
-                    overflow: "hidden",
-                    cursor: "pointer",
-                  }}
-                  variant="h5"
-                  color="white"
-                  onClick={() => setShowPassword((prev) => !prev)}
-                >
-                  {showPassword
-                    ? generatedPassword
-                    : new Array(generatedPassword.length).fill("∗")}
-                </Typography>
-              </Tooltip>
+          Generate
+        </Button>
 
-              <Tooltip title="Copy">
-                <IconButton
-                  color="success"
-                  onClick={() => CopyToClipboard(generatedPassword)}
-                >
-                  <ContentCopyICon />
-                </IconButton>
-              </Tooltip>
-            </Box>
-          ) : (
-            <Typography variant="h5" color={"white"}>
-              Password generator
-            </Typography>
-          )}
-        </Box>
-
-        <Box
-          sx={{
-            display: "flex",
-            gap: 2,
-            width: "fit-content",
-            marginInline: "auto",
-          }}
-        >
-          <Button
-            sx={{ width: "fit-content" }}
-            variant="contained"
-            color="primary"
-            onClick={generatePassword}
-          >
-            Generate
-          </Button>
-
+        <Tooltip title="Settings">
           <IconButton
             sx={{ marginInline: "auto" }}
             size="large"
-            onClick={() => setIsSettingOpened((prev) => !prev)}
+            onClick={() => setIsSettingOpened(!isSettingOpened)}
           >
             <SettingsIcon />
           </IconButton>
-        </Box>
-
-        <SettingsMenu
-          open={isSettingOpened}
-          onClose={() => setIsSettingOpened(false)}
-          passwordLength={passwordLength}
-          onChangePasswordLength={(newLength) => setPasswordLength(newLength)}
-          charactersType={charactersType}
-          onChangeCharactersType={(newType) => setCharactersType(newType)}
-        />
+        </Tooltip>
       </Box>
-    </Box>
+
+      <SettingsMenu
+        open={isSettingOpened}
+        onClose={() => setIsSettingOpened(false)}
+      />
+    </Layout>
   );
 };
 
