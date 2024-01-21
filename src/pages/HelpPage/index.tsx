a2e076ffa2aed63976f7765d0b3c532c1a4d68bb
KostiaTrepyk <137499840+KostiaTrepyk@users.noreply.github.com>
2024-01-21 14:58:57 +0100

    Help page

---
+++ src/pages/HelpPage/index.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { Box, IconButton, Tooltip, Typography } from "@mui/material";
+import { ArrowBackIosNew } from "@mui/icons-material";
+
+import { useRouterContext } from "../../core/router/hooks/useRouterContext";
+
+const HelpPage = () => {
+  const { navigate } = useRouterContext();
+  return (
+    <>
+      <Box sx={{ position: "relative" }}>
+        <Tooltip title="Back">
+          <IconButton sx={{ m: 1 }} onClick={() => navigate("/")}>
+            <ArrowBackIosNew />
+          </IconButton>
+        </Tooltip>
+      </Box>
+
+      <Box sx={{ m: 4, display: "flex", flexDirection: "column", gap: 1.5 }}>
+        <Typography sx={{ mb: 1 }} variant="h5">
+          Keyboard
+        </Typography>
+
+        <Typography>
+          <Typography sx={{ color: "#2bf" }} component="span">
+            H
+          </Typography>{" "}
+          - help.
+        </Typography>
+
+        <Typography>
+          <Typography sx={{ color: "#2bf" }} component="span">
+            S
+          </Typography>{" "}
+          - show/hide password.
+        </Typography>
+
+        <Typography>
+          <Typography sx={{ color: "#2bf" }} component="span">
+            G
+          </Typography>{" "}
+          - generate password.
+        </Typography>
+
+        <Typography>
+          <Typography sx={{ color: "#2bf" }} component="span">
+            Space
+          </Typography>{" "}
+          - show/hide settings.
+        </Typography>
+
+        <Typography>
+          <Typography sx={{ color: "#2bf" }} component="span">
+            M
+          </Typography>{" "}
+          - main page.
+        </Typography>
+      </Box>
+    </>
+  );
+};
+
+export default HelpPage;
